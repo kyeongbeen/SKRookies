c4e00a0bce9719735e5fedea803fa2583bf0f71c
KyeongBin <kyeongbin0205@naver.com>
2024-12-03 15:18:43 +0900

    feat: sk Shieldus Rookies 2일차
    JavaScript test, variable 파일 추가
    
    2024-12-03

---
+++ frontend/js/1_var.js
@@ -0,0 +1,17 @@
+
+// 1. 상수 (const)
+const PI = 3.14;
+console.log(PI);
+
+// 상수값 수정 -> Runtime Error -> 상수값을 변경시도해서 발생
+// TypeError: Assignment to constant variable.
+// PI = 3.141592;
+// console.log(PI);
+
+// 2. 변수 (var or let)
+var a = 1;
+console.log(a);
+
+
+
+
